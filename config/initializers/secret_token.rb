0d4084ce90997bf1f9f8cb175f90b5826d583242
EshaMukherjee <vinsol@EshaMukherjee-MacBook-Pro.local>
2011-11-29 10:17:49 +0530

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EshaVIMapp::Application.config.secret_token = 'f2dc519b3a7be1d7b70129e9694ab33e1611dfd32851c20198f39ef05285ea1db191f2704282913b80989088ff703d408804f0a90111428e102a349622610dab'
