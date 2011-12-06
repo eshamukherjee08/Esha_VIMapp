5b99e813022e06ca34fe947b926f6a3428eeb127
EshaMukherjee <vinsol@EshaMukherjee-MacBook-Pro.local>
2011-12-06 17:53:05 +0530

    commit master to switch

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :admin
-  has_many :batches, :dependent => :destroy
+  has_many :batches
   accepts_nested_attributes_for :batches, :allow_destroy => true
   #has_and_belongs_to_many :candidates
   validates_associated :batches
